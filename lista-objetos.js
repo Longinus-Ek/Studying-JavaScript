8f3ea18c4ce7c327e8a0fd21f6ead3f91007f663
Erick Dias <derickbass4@gmail.com>
2022-07-25 14:47:29 -0300

    Algoritmos II

---
+++ lista-objetos.js
@@ -0,0 +1,56 @@
+
+const Livros1 = [
+{
+    titulo: "JavaScript",
+    preco: 25
+}, 
+{
+    titulo: "PHP",
+    preco: 15
+},
+{
+    titulo: "Java",
+    preco: 30
+},
+{
+    titulo:"Elixir",
+    preco: 50
+},
+{
+    titulo: "Go",
+    preco: 45
+}, 
+{   
+    titulo: "Python",
+    preco: 20
+}
+]
+
+const Livros2 = [
+{
+    titulo: "SQLServer",
+    preco: 35
+}, 
+{
+    titulo: "SQLite",
+    preco: 15
+},
+{
+    titulo: "PostgreeDB",
+    preco: 25
+},
+{
+    titulo:"OracleDB",
+    preco: 60
+},
+{
+    titulo: "MySQL",
+    preco: 50
+}, 
+{   
+    titulo: "MongoDB",
+    preco: 20
+}
+]
+
+module.exports = {Livros1, Livros2}
